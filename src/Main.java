3589826c688db56c515af7138e674c5608021d55
Emosans <shreyasd1414@gmail.com>
2024-10-06 19:35:55 +0530

    simple backtracking method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,31 @@ public class Main {
         int[][] su = new int[9][9];
         Su suBoard = new Su(su);
 
+        long cTime = System.currentTimeMillis();
+
+        System.out.println(cTime);
+
+        // fill random nos first
+        suBoard.fillBoardWithRandomNumbers(20);
+        System.out.println("Board with random nos before solving : \n");
+
         suBoard.drawBoard();
+
+        System.out.println("");
+
+        System.out.println("After solving : \n");
+
+        // return solved board
+        if(suBoard.SuSolved(suBoard.getSu())){
+            System.out.println("Sol exists");
+            suBoard.drawBoard();
+        } else {
+            System.out.println("no soln exists");
+        }
+
+        // suBoard.drawBoard();
+        
+        long eTime = System.currentTimeMillis();
+        System.out.println(eTime);
     }
 }
--- src/struct/Su.java
+++ src/struct/Su.java
@@ -17,25 +17,28 @@ public class Su {
         this.sudokuBoard = su;
     }
 
-    public void drawBoard(){
-
-        // max random numbers
-        int maxRandoms = 10;
-
+    public void fillBoardWithRandomNumbers(int maxRandoms) {
         Random random = new Random();
 
-        for(i=0;i<maxRandoms;i++){
-            int row,col,num;
+        for (int i = 0; i < maxRandoms; i++) {
+            int row, col, num;
 
-            do{
+            do {
                 row = random.nextInt(9);
-                col  = random.nextInt(9);
-            } while(sudokuBoard[row][col] != 0); // repeat if board pos is not empty (0)
+                col = random.nextInt(9);
+            } while (sudokuBoard[row][col] != 0); // repeat if board pos is not empty (0)
 
-            num = random.nextInt(9) + 1;
-
-            sudokuBoard[row][col] = num;
+            // generate a random number and check its validity
+            for (num = 1; num <= 9; num++) {
+                if (isRowColValueValid(sudokuBoard, row, col, num)) {
+                    sudokuBoard[row][col] = num; // assign valid random number
+                    break; 
+                }
+            }
         }
+    }
+
+    public void drawBoard(){
 
         for (i=0;i<9;i++){
             for (j=0;j<9;j++){
@@ -43,5 +46,61 @@ public class Su {
             }
             System.out.println("");
         }
+    }
+
+    public boolean SuSolved(int[][] sudokuBoard){
+
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+
+                // check for empty space (0)
+                if(sudokuBoard[row][col] == 0){
+                    // try to solve
+                    // check the row and column of the first occurence of empty space
+                    // place the valid num
+                    for(int num=1;num<=9;num++){
+                        if(isRowColValueValid(sudokuBoard,row,col,num)){
+                            // assign the num if valid
+                            sudokuBoard[row][col] = num;
+
+                            // check func recursively
+                            if(SuSolved(sudokuBoard)){
+                                return true; //solved!
+                            }
+
+                            sudokuBoard[row][col] = 0; // undo all moves at pos row,col since it doesnt solve the puzzle
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isRowColValueValid(int[][] su,int row,int col, int num){
+
+        for(int x=0;x<9;x++){
+            if(su[row][x] == num || su[x][col] == num){ // checking the whole row and column
+                return false;
+            }
+        }
+
+        int one_cell_row = row - row % 3;
+        int one_cell_col = col - col % 3;
+
+        // check and assign num in 3x3 grid
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                if(su[i+one_cell_row][j+one_cell_col] == num){
+                    return false; // check in the cell that is (0,0),(0,1),(0,2) and compare with num
+                }
+            }
+        }
+        return true;
+    }
+
+    public int[][] getSu(){
+        return sudokuBoard;
     }
 }

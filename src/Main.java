214cb9c905149629d7f6db060fc8077583a3364d
Emosans <shreyasd1414@gmail.com>
2024-10-07 15:32:33 +0530

    defined a flag for simple backtrack

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,47 @@ import struct.Su;
 
 public class Main {
     public static void main(String[] args){
+
+        boolean naiveBacktrack = false;
+
         int[][] su = new int[9][9];
         Su suBoard = new Su(su);
 
-        long cTime = System.currentTimeMillis();
 
-        System.out.println(cTime);
-
-        // fill random nos first
-        suBoard.fillBoardWithRandomNumbers(20);
-        System.out.println("Board with random nos before solving : \n");
-
-        suBoard.drawBoard();
-
-        System.out.println("");
-
-        System.out.println("After solving : \n");
-
-        // return solved board
-        if(suBoard.SuSolved(suBoard.getSu())){
-            System.out.println("Sol exists");
-            suBoard.drawBoard();
-        } else {
-            System.out.println("no soln exists");
+        for(String arg: args){
+            if(arg.equals("-nb")){
+                naiveBacktrack=true;
+            }
         }
 
-        // suBoard.drawBoard();
-        
-        long eTime = System.currentTimeMillis();
-        System.out.println(eTime);
+        if(naiveBacktrack){
+            long cTime = System.currentTimeMillis();
+    
+            System.out.println(cTime);
+    
+            // fill random nos first
+            suBoard.fillBoardWithRandomNumbers(20);
+            System.out.println("Board with random nos before solving : \n");
+    
+            suBoard.drawBoard();
+    
+            System.out.println("");
+    
+            System.out.println("After solving : \n");
+    
+            // return solved board
+            if(suBoard.SuSolved(suBoard.getSu())){
+                System.out.println("Sol exists");
+                suBoard.drawBoard();
+            } else {
+                System.out.println("no soln exists");
+            }
+    
+            // suBoard.drawBoard();
+            
+            long eTime = System.currentTimeMillis();
+            long timeTaken = eTime-cTime;
+            System.out.println("time taken : "+timeTaken+" ms");
+        }
     }
 }
